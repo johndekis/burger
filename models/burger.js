6007ffc4947bec160ea5ad315a91e1a55616d752
John Dekis <johndekis@gmail.com>
2018-03-26 13:10:59 -0400

    filled out the structure

--- models/burger.js
+++ models/burger.js
@@ -1,5 +1,28 @@
-var orm = require('./config/orm.js');
+var orm = require('../config/orm.js');
 
 //call orm functions with burger specific input for the orm
+var burger = {
+    selectAll: function(cb) {
+        orm.selectAll("burgers", function(res) {
+          cb(res);
+        });
+    },
+    insertOne:  function(cols, vals, cb) {
+        orm.insertOne("burgers", cols, vals, function(res) {
+          cb(res);
+        });
+    },
+    updateOne: function(objColVals, condition, cb) {
+        orm.update("burgers", objColVals, condition, function(res) {
+          cb(res);
+        });
+    },
+    delete: function(condition, cb) {
+        orm.delete("burgers", condition, function(res) {
+          cb(res);
+        });
+    }
+}
 
-module.exports = './burger.js';
+
+module.exports = burger;
--- server.js
+++ server.js
@@ -1,2 +1,33 @@
 var express = require("express");
 var bodyParser = require("body-parser");
+var exphbs = require("express-handlebars");
+var app = express();
+
+// Sets initial port. 
+var PORT = process.env.PORT || 8080;
+
+// handle data parsing
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+//allows public folder to serve static files
+app.use(express.static('/public'));
+
+// Set Handlebars.
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+
+//ROUTES=============================================================
+
+var routes = require("./controllers/burgers_controller.js");
+
+app.use(routes);
+
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+  
+});
