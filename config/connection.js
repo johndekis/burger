7846456b55bc01d6753958c200049d8ec54a2933
John Dekis <johndekis@gmail.com>
2018-03-28 15:44:53 -0400

    gave up the cc #

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,20 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
+var connection;
+
+if(process.env.JAWS_URL){
+  connection = mysql.createConnection(process.env.JAWS_URL);
+} else{
+  connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
     password: "root",
     database: "burgers_db"
   });
+}
+
+
 
   connection.connect(function(err) {
     if (err) {
